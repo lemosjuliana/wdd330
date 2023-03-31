dec9b6bb7f6e5edbf23f4af2ea607dc30ef4f6b1
lemosjuliana <lemos.julianajesus@gmail.com>
2023-03-31 09:47:00 -0300

    index fix info

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -11,3 +11,16 @@ const list = new ProductList(category, dataSource, element);
 
 list.init();
 
+// // maybe use this...? 
+
+// import ExternalServices from "./ExternalServices.mjs";
+// import ProductDetails from "./ProductDetails.mjs";
+// import { getParam, loadHeaderFooter } from "./utils.mjs";
+
+// loadHeaderFooter();
+
+// const productId = getParam("product");
+// const dataSource = new ExternalServices("tents");
+
+// const product = new ProductDetails(productId, dataSource);
+// product.init();
